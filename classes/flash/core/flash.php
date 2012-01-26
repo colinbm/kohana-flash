ea4d4956c55fa7d71c04d3c39896f954e96bd471
Colin Burn-Murdoch <colin@burn-murdoch.com>
2012-01-26 10:52:35 +0000

    Define methods as static

--- classes/flash/core/flash.php
+++ classes/flash/core/flash.php
@@ -8,7 +8,7 @@ class Flash_Core_Flash
 	const ERROR   = 'error';
 	const WARNING = 'warning';
 	
-	public function add($message, $type=self::INFO) {
+	public static function add($message, $type=self::INFO) {
 		$session = Session::instance();
 		$flashes = $session->get('flashes');
 		if (!$flashes) $flashes = array();
@@ -16,41 +16,41 @@ class Flash_Core_Flash
 		$session->set('flashes', $flashes);
 	}
 	
-	public function success($message) {
+	public static function success($message) {
 		self::add($message, self::SUCCESS);
 	}
 	
-	public function info($message) {
+	public static function info($message) {
 		self::add($message, self::INFO);
 	}
 	
-	public function error($message) {
+	public static function error($message) {
 		self::add($message, self::ERROR);
 	}
 	
-	public function warning($message) {
+	public static function warning($message) {
 		self::add($message, self::WARNING);
 	}
 	
-	public function get() {
+	public static function get() {
 		$session = Session::instance();
 		$flashes = $session->get('flashes');
 		if (!$flashes) $flashes = array();
 		return $flashes;
 	}
 	
-	public function get_once() {
+	public static function get_once() {
 		$flashes = self::get();
 		self::clear();
 		return $flashes;
 	}
 	
-	public function clear() {
+	public static function clear() {
 		$session = Session::instance();
 		$session->set('flashes', array());
 	}
 	
-	public function render() {
+	public static function render() {
 		$view = View::factory('flash/flash');
 		$view->flashes = self::get_once();
 		return $view->render();
